65f0bf808dd483e21120d7d2454e69943a9511a4
Ahmed Waheed <mrcreamio@outlook.com>
2023-08-17 13:28:58 +0500

    updated the code with class tests

--- tests/unit/test_functions.py
+++ tests/unit/test_functions.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_add_positive_numbers():
     assert add_numbers(1, 2) == 3
-    
+   
 @pytest.mark.end_to_end
 def test_add_negative_numbers():
     assert add_numbers(-1, -2) == -3
@@ -12,11 +12,11 @@ def test_add_negative_numbers():
 def test_add_numbers_zero():
     assert add_numbers(0, 0) == 0
 
-@pytest.mark.unit
-def test_multiply_numbers():
-    assert multiply_numbers(2, 3) == 6
+class TestsUnit:
+    @pytest.mark.unit
+    def test_multiply_numbers(self):
+        assert multiply_numbers(2, 3) == 6
 
-
-@pytest.mark.unit
-def test_multiply_numbers_zero():
-    assert multiply_numbers(0, 0) == 0
+    @pytest.mark.unit
+    def test_multiply_numbers_zero(self):
+        assert multiply_numbers(0, 0) == 0
